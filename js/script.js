bb2156d1b5d57f94a5fb1f02b9e17816fa5150f8
Lou <mantra2406@gmail.com>
2021-05-19 17:04:56 -0400

    Fixed main bug in the prices display.
    Fixed bug on the reset function, no reseting all the prices.
    
    Added instructions to follow the next time to improve the app.
    
    Deleted comments on JS files.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+/* 
+  todo Limpiar Lineas CSS sin usar.
+  todo Actualizar estilos CSS de la calculadora 2
+*/
+
 const precioBolivares = document.getElementById('price-calc');
 const calcularPrecio = document.getElementById('calc-btn');
 const resetValues = document.getElementById('reset-btn');
@@ -11,64 +16,7 @@ const menuSection = document.getElementById('menu-section');
 const openMenuBtn = document.getElementById('open-menu-btn');
 const closeMenuBtn = document.getElementById('close-menu-btn');
 
-const preciosDolares = {
-  // Aceite_Soya: 2.1,
-  // Arroz_Emi: 1,
-  // Arroz_Palacio: 1.1,
-  // Arroz_Royal: 1,
-  // Atun: 1,
-  // Azucar_Uniao: 0.8,
-  // Azucar_Montalban: 0.8,
-  // Bombillos: 0.5,
-  // Cafe_madrid_250: 1.5,
-  // Cafe_Madrid_500: 2.7,
-  // Carton_de_Huevos: 3.5,
-  // Chimo: 0.4,
-  // Condimentos: 0.4,
-  // Crema_dental_Alident: 0.7,
-  // Crema_dental_Colgate: 0.8,
-  // Desodorante: 0.5,
-  // Fructus: 0.3,
-  // Harina_Dona_Belen: 1.1,
-  // Harina_Dona_Gema: 1,
-  // Harina_Levudante: 1.3,
-  // Harina_pan: 1.3,
-  // Hojillas_Schick: 0.3,
-  // Hojillas_Schick_cu: 0.1,
-  // Jabon_Azul: 0.6,
-  // Jabon_de_Olor: 0.5,
-  // Konga: 0.5,
-  // Leche_DoBon: 1.3,
-  // Mantequilla_Adorita: 0.7,
-  // Mantequilla_Estancia: 1.2,
-  // Mayonesa_445gr: 1.7,
-  // Mayonesa_Mavesa: 1.3,
-  // Mortadela: 2,
-  // Papel_Higienico_Rosal_paquete: 1,
-  // Papel_Higienico_Rosal_cu: 0.3,
-  // Pasta_1kg_Larga_y_Corta: 1.3,
-  // Pasta_500gr_Larga_y_Corta: 0.7,
-  // Pasta_Venecia: 1.2,
-  // Pega_loca: 0.5,
-  // Pilas_CU: 0.4,
-  // Pilas_paquete: 0.9,
-  // Queso_Kg: 2.7,
-  // Sal: 0.3,
-  // Salchichas: 1.7,
-  // Salsa_de_ajo: 0.9,
-  // Salsa_de_Pasta: 0.8,
-  // Salsa_de_tomate: 1,
-  // Salsa_Inglesa: 0.9,
-  // Salsa_Soya: 0.9,
-  // Sardinas: 0.9,
-  // Shampoo: 0.4,
-  // Sopa_Maggi: 1,
-  // Tabaco: 0.3,
-  // Velas: 0.3,
-  // Vinagre: 0.9,
-  // Yesquero: 0.5,
-};
-
+// Selection of the multiple boxes for the prices
 const priceList1 = document.getElementById('price-ves1');
 const priceList2 = document.getElementById('price-ves2');
 const priceList3 = document.getElementById('price-ves3');
@@ -102,16 +50,6 @@ const price13 = { price13: [pricePasta] };
 const price14 = { price14: [priceFish] };
 const price15 = { price15: [priceSalsa] };
 const price16 = { price16: [priceOthers] };
-
-/* 
-todo CREAR UNA NUEVA FUNCION PARA CALCULAR LOS PRECIOS DE CADA CATEGORIA y crear los contenedores de esos precios.
-todo AGREGRAR EL RESTO DE CATEGORIAS hechas en el documento word.
-todo AGREGRAR NUEVA CATEGORIA "DULCES" con sus respectivos precios
-todo Limpiar Lineas CSS sin usar.
-todo Actualizar estilos CSS de la calculadora 2
-todo Arreglar estilos del titulo principal y el boton del menu, para que el titulo quede en el medio y el boton del menu de lado.
-
-*/
 
 const calcular = () => {
   let chars1 = price1['price1'];
@@ -295,6 +233,21 @@ const calcular = () => {
 };
 const reset = () => {
   priceList1.innerHTML = '';
+  priceList2.innerHTML = '';
+  priceList3.innerHTML = '';
+  priceList4.innerHTML = '';
+  priceList5.innerHTML = '';
+  priceList6.innerHTML = '';
+  priceList7.innerHTML = '';
+  priceList8.innerHTML = '';
+  priceList9.innerHTML = '';
+  priceList10.innerHTML = '';
+  priceList11.innerHTML = '';
+  priceList12.innerHTML = '';
+  priceList13.innerHTML = '';
+  priceList14.innerHTML = '';
+  priceList15.innerHTML = '';
+  priceList16.innerHTML = '';
   spanPrice.innerHTML = '';
   textPrice.style.opacity = '0';
   precioBolivares.value = '';
